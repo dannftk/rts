e4d248159b05d6a302efaf8320ac07a8233da545
dannftk <dannftk@yandex.ru>
2013-07-01 21:48:04 +0400

    fix tasks, handlers are clear

--- gateway/src/gateway_tasks.c
+++ gateway/src/gateway_tasks.c
@@ -28,28 +28,34 @@ extern int g_vector_res_rec_count[VECTOR_SIZE];
 
 void gateway_tasks_put_mtrx_A_value(mtrx_fmt_t mtrx)
 {
-    task_data_t task_data = {
-        .task_type = MUL_MTRX_A_VALUE_ON_VEC_b_VALUE,
-        .data.mtrx_val_pos = mtrx.pos
-    };
-
     g_mtrx_A[mtrx.pos.row][mtrx.pos.col] = mtrx.value;
     g_mtrx_A_written[mtrx.pos.row][mtrx.pos.col] = 1;
 
-    gateway_scheduler_add_task_to_queue(task_data);
+    if (g_vector_b_written[mtrx.pos.col])
+    {
+        task_data_t task_data = {
+            .task_type = MUL_MTRX_A_VALUE_ON_VEC_b_VALUE,
+            .data.mtrx_val_pos = mtrx.pos
+        };
+
+        gateway_scheduler_add_task_to_queue(task_data);
+    }
 }
 
 void gateway_tasks_put_mtrx_C_value(mtrx_fmt_t mtrx)
 {
-    task_data_t task_data = {
-        .task_type = MUL_MTRX_C_VALUE_ON_VEC_d_VALUE,
-        .data.mtrx_val_pos = mtrx.pos
-    };
-
     g_mtrx_C[mtrx.pos.row][mtrx.pos.col] = mtrx.value;
     g_mtrx_C_written[mtrx.pos.row][mtrx.pos.col] = 1;
 
-    gateway_scheduler_add_task_to_queue(task_data);
+    if (g_vector_d_written[mtrx.pos.col])
+    {
+        task_data_t task_data = {
+            .task_type = MUL_MTRX_C_VALUE_ON_VEC_d_VALUE,
+            .data.mtrx_val_pos = mtrx.pos
+        };
+
+        gateway_scheduler_add_task_to_queue(task_data);
+    }
 }
 
 void gateway_tasks_put_vector_b_value(vector_fmt_t vector)
@@ -80,33 +86,41 @@ void gateway_tasks_put_vector_d_value(vector_fmt_t vector)
 
 void gateway_tasks_mul_mtrx_A_val_on_vector_b_val(mtrx_val_pos_t mtrx_val_pos)
 {
-    g_vector_Ab_res[mtrx_val_pos.row] += g_mtrx_A[mtrx_val_pos.row][mtrx_val_pos.col] * g_vector_b[mtrx_val_pos.col];
-    ++g_vector_Ab_res_rec_count[mtrx_val_pos.row];
-
-    if (COLUMNS == g_vector_Ab_res_rec_count[mtrx_val_pos.row])
+    if (1 == g_mtrx_A_written[mtrx_val_pos.row][mtrx_val_pos.col])
     {
-        task_data_t task_data = {
-            .task_type = ADD_RES_Ab_VECTOR_VALUE_TO_RES_VECTOR_VALUE,
-            .data.value_position = mtrx_val_pos.row
-        };
+        g_vector_Ab_res[mtrx_val_pos.row] += g_mtrx_A[mtrx_val_pos.row][mtrx_val_pos.col] * g_vector_b[mtrx_val_pos.col];
+        g_mtrx_A_written[mtrx_val_pos.row][mtrx_val_pos.col] = 2;
+        ++g_vector_Ab_res_rec_count[mtrx_val_pos.row];
 
-        gateway_scheduler_add_task_to_queue(task_data);
+        if (COLUMNS == g_vector_Ab_res_rec_count[mtrx_val_pos.row])
+        {
+            task_data_t task_data = {
+                .task_type = ADD_RES_Ab_VECTOR_VALUE_TO_RES_VECTOR_VALUE,
+                .data.value_position = mtrx_val_pos.row
+            };
+
+            gateway_scheduler_add_task_to_queue(task_data);
+        }
     }
 }
 
 void gateway_tasks_mul_mtrx_C_val_on_vector_d_val(mtrx_val_pos_t mtrx_val_pos)
 {
-    g_vector_Cd_res[mtrx_val_pos.row] += g_mtrx_C[mtrx_val_pos.row][mtrx_val_pos.col] * g_vector_d[mtrx_val_pos.col];
-    ++g_vector_Cd_res_rec_count[mtrx_val_pos.row];
-
-    if (COLUMNS == g_vector_Cd_res_rec_count[mtrx_val_pos.row])
+    if (1 == g_mtrx_C_written[mtrx_val_pos.row][mtrx_val_pos.col])
     {
-        task_data_t task_data = {
-            .task_type = ADD_RES_Cd_VECTOR_VALUE_TO_RES_VECTOR_VALUE,
-            .data.value_position = mtrx_val_pos.row
-        };
+        g_vector_Cd_res[mtrx_val_pos.row] += g_mtrx_C[mtrx_val_pos.row][mtrx_val_pos.col] * g_vector_d[mtrx_val_pos.col];
+        g_mtrx_C_written[mtrx_val_pos.row][mtrx_val_pos.col] = 2;
+        ++g_vector_Cd_res_rec_count[mtrx_val_pos.row];
 
-        gateway_scheduler_add_task_to_queue(task_data);
+        if (COLUMNS == g_vector_Cd_res_rec_count[mtrx_val_pos.row])
+        {
+            task_data_t task_data = {
+                .task_type = ADD_RES_Cd_VECTOR_VALUE_TO_RES_VECTOR_VALUE,
+                .data.value_position = mtrx_val_pos.row
+            };
+
+            gateway_scheduler_add_task_to_queue(task_data);
+        }
     }
 }
 
@@ -116,9 +130,10 @@ void gateway_tasks_mul_vec_b_value_on_column_mtrx_A(int vector_val_pos)
 
     for (row = 0; row < ROWS; row++)
     {
-        if (g_mtrx_A_written[row][vector_val_pos])
+        if (1 == g_mtrx_A_written[row][vector_val_pos])
         {
             g_vector_Ab_res[row] += g_mtrx_A[row][vector_val_pos] * g_vector_b[vector_val_pos];
+            g_mtrx_A_written[row][vector_val_pos] = 2;
             ++g_vector_Ab_res_rec_count[row];
 
             if (COLUMNS == g_vector_Ab_res_rec_count[row])
@@ -140,9 +155,10 @@ void gateway_tasks_mul_vec_d_value_on_column_mtrx_C(int vector_val_pos)
 
     for (row = 0; row < ROWS; row++)
     {
-        if (g_mtrx_C_written[row][vector_val_pos])
+        if (1 == g_mtrx_C_written[row][vector_val_pos])
         {
             g_vector_Cd_res[row] += g_mtrx_C[row][vector_val_pos] * g_vector_d[vector_val_pos];
+            g_mtrx_C_written[row][vector_val_pos] = 2;
             ++g_vector_Cd_res_rec_count[row];
 
             if (COLUMNS == g_vector_Cd_res_rec_count[row])
