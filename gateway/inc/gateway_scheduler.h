10b764e2f401c5d5efead2a5753e8e13bc3c6806
dannftk <dannftk@yandex.ru>
2013-08-03 14:22:15 +0400

    replace tabs by spaces

--- gateway/inc/gateway_scheduler.h
+++ gateway/inc/gateway_scheduler.h
@@ -6,8 +6,8 @@
 #define TASK_QUEUE_SIZE 50
 
 typedef struct task_queue_s {
-	task_data_t task_data;
-	unsigned int priority;
+    task_data_t task_data;
+    unsigned int priority;
 } task_queue_t;
 
 void gateway_scheduler_add_task_to_queue(task_data_t task_data);
